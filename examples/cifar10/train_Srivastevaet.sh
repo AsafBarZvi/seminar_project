c06726acfe36f07e5d0e7dfd47394c38cfffe260
Asaf <bar.zvi.asaf@gmail.com>
2016-12-29 13:01:10 +0200

    Comment the weight flage

--- examples/cifar10/train_Srivastevaet.sh
+++ examples/cifar10/train_Srivastevaet.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 set -e
 
-TOOLS=./build/tools
+TOOLS=./build/tools/release
 
 $TOOLS/caffe train \
     --solver examples/cifar10/cifar10_Srivastavaet_solver_4.prototxt \
-    --weights examples/cifar10/cifar10_Srivastavaet_iter_60000.caffemodel.h5 \
-    |& tee examples/cifar10/cifar10_Srivastavaet.log 
+    #--weights examples/cifar10/cifar10_Srivastavaet_iter_60000.caffemodel.h5 \
+    > examples/cifar10/cifar10_Srivastavaet.log 
